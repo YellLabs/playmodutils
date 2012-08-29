557cafc5a81c081c93462687791d47e1f9fe368b
Paolo Gentili <gentilipaolo@gmail.com>
2012-08-29 14:30:06 +0100

    moved in template extensions and added dateFormat extension to display timestamps in UTC time

---
+++ app/com/yell/civitas/play/utils/template/YellTemplateExtensions.java
@@ -0,0 +1,30 @@
+package com.yell.civitas.play.utils.template;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import play.Play;
+import play.templates.JavaExtensions;
+
+public class YellTemplateExtensions extends JavaExtensions {
+
+    public static String escapeJavaScriptYell(String str) {
+    	// call standard string escape method
+    	String escapedString = escapeJavaScript(str);
+    	// now replace and escaped single quotes with a non-escaped single quote
+    	String nonEscaped = escapedString.replace("\\'", "'");
+        return nonEscaped;
+    }
+    
+    public static String dateFormat(Date date) {
+    	if (date==null) 
+    		return null;
+    	
+    	// the date format defaults to ISO8601 with ±HHMM offset
+    	String iso8601Pattern = Play.configuration.getProperty("date.format", "yyyy-MM-dd'T'HH:mm:ssZ");
+    	
+    	SimpleDateFormat df = new SimpleDateFormat(iso8601Pattern);
+    	df.setTimeZone(TimeZone.getTimeZone("UTC"));
+    	return df.format(date);
+    }
+}
